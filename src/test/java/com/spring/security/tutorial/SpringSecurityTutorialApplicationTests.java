41a9733fb57eebbd7804e1571687062a8d72f86b
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-02-20 12:35:27 -0500

    initial project

---
+++ src/test/java/com/spring/security/tutorial/SpringSecurityTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
