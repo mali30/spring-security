9716cadff24041b61375bcdaf4acd89908165c94
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-02-20 12:34:28 -0500

    added student controller

---
+++ src/main/java/com/spring/security/tutorial/controller/StudentController.java
@@ -0,0 +1,29 @@
+package com.spring.security.tutorial.controller;
+
+import com.spring.security.tutorial.model.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class StudentController {
+
+    private static final List<Student> students = Arrays.asList(
+            new Student(1, "James Bond"),
+            new Student(2, "Dora Explorer"),
+            new Student(3, "Bruce Wayne"));
+
+    @GetMapping(path = "/{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId){
+        return students.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow( () -> new IllegalStateException(
+                        "student " + studentId + " does not exist"));
+    }
+}
