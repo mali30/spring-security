0968e415bd6e434f73335440d51eba966472ad6f
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-02-21 13:59:47 -0500

    added admin_traine role, added method to get permissions for a user

--- src/main/java/com/spring/security/tutorial/security/Roles.java
+++ src/main/java/com/spring/security/tutorial/security/Roles.java
@@ -1,8 +1,10 @@
 package com.spring.security.tutorial.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public enum Roles {
     STUDENT(Sets.newHashSet()),
@@ -10,6 +12,10 @@ public enum Roles {
             UserPermissions.COURSE_READ,
             UserPermissions.COURSE_WRITE,
             UserPermissions.STUDENT_WRITE,
+            UserPermissions.STUDENT_READ
+    )),
+    ADMIN_TRAINEE(Sets.newHashSet(
+            UserPermissions.COURSE_READ,
             UserPermissions.STUDENT_READ
     ));
 
@@ -19,7 +25,17 @@ public enum Roles {
         this.permissions = permissions;
     }
 
-    public Set<UserPermissions> permissions() {
+    public Set<UserPermissions> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthority() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+
         return permissions;
     }
 }
